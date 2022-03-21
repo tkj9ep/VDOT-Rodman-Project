78a985ab1e083e802abc0b9393f53d1c67f0895b
tkj9ep <tkj9ep@virginia.edu>
2022-03-21 15:16:43 -0400

    stuff

--- anenometerTemplate/anenometerTemplate.ino
+++ anenometerTemplate/anenometerTemplate.ino
@@ -1,77 +1,51 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+const int sensorPin = A0; //Defines the pin that the anemometer output is connected to
+int sensorValue = 0; //Variable stores the value direct from the analog pin
+float sensorVoltage = 0; //Variable that stores the voltage (in Volts) from the anemometer being sent to the analog pin
+float windSpeed = 0; // Wind speed in meters per second (m/s)
  
-#define SCREEN_WIDTH 128  // OLED display width, in pixels
-#define SCREEN_HEIGHT 64  // OLED display height, in pixels
-#define OLED_RESET    4  // Reset pin # (or -1 if sharing reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+float voltageConversionConstant = .004882814; //This constant maps the value provided from the analog read function, which ranges from 0 to 1023, to actual voltage, which ranges from 0V to 5V
+int sensorDelay = 1000; //Delay between sensor readings, measured in milliseconds (ms)
+ 
+//Anemometer Technical Variables
+//The following variables correspond to the anemometer sold by Adafruit, but could be modified to fit other anemometers.
+ 
+float voltageMin = .43; // Mininum output voltage from anemometer in mV.
+float windSpeedMin = 0; // Wind speed in meters/sec corresponding to minimum voltage
+ 
+float voltageMax = 2.0; // Maximum output voltage from anemometer in mV.
+float windSpeedMax = 32; // Wind speed in meters/sec corresponding to maximum voltage
  
  
-void setup()
-{
-  Serial.begin(9600);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;); // Don't proceed, loop forever
-  }
-  display.display();
-  delay(100);
-  display.clearDisplay();
  
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(2);
-  display.setCursor(0, 20);
-  display.print("Anemometer");
-  display.display();
-  delay(3000);
+void setup() 
+{              
+  Serial.begin(9600);  //Start the serial connection
 }
  
-void loop()
+ 
+void loop() 
 {
-  float sensorValue = analogRead(A0);
-  Serial.print("Analog Value =");
-  Serial.println(sensorValue);
+sensorValue = analogRead(sensorPin); //Get a value between 0 and 1023 from the analog pin connected to the anemometer
  
-  float voltage = (sensorValue / 1023) * 5;
-  Serial.print("Voltage =");
-  Serial.print(voltage);
-  Serial.println(" V");
+sensorVoltage = sensorValue * voltageConversionConstant; //Convert sensor value to actual voltage
  
-  float wind_speed = mapfloat(voltage, 0.4, 2, 0, 32.4);
-  float speed_mph = ((wind_speed *3600)/1609.344);
-  Serial.print("Wind Speed =");
-  Serial.print(wind_speed);
-  Serial.println("m/s");
-  Serial.print(speed_mph);
-  Serial.println("mph");
- 
-  display.clearDisplay();
- 
-  display.setTextSize(1);
-  display.setCursor(30, 0);
-  display.println("Wind Speed");
- 
-//  display.setTextSize(2);
-//  display.setCursor(25, 30);
-//  display.print(wind_speed, 1);
-//  display.setTextSize(1);
-//  display.print(" m/s");
- 
-  display.setTextSize(2);
-  display.setCursor(25, 30);
-  display.print(speed_mph, 1);
-  display.setTextSize(1);
-  display.print(" mph");
- 
-  display.display();
- 
-  Serial.println(" ");
-  delay(300);
+//Convert voltage value to wind speed using range of max and min voltages and wind speed for the anemometer
+if (sensorVoltage <= voltageMin){
+ windSpeed = 0; //Check if voltage is below minimum value. If so, set wind speed to zero.
+}else {
+  windSpeed = (sensorVoltage - voltageMin)*windSpeedMax/(voltageMax - voltageMin); //For voltages above minimum value, use the linear relationship to calculate wind speed.
 }
  
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+ //Print voltage and windspeed to serial
+  Serial.print("Voltage: ");
+  Serial.print(sensorVoltage);
+  Serial.print("\t"); 
+  Serial.print("Wind speed: ");
+  Serial.println(mphConversion(windSpeed)); 
+ 
+ delay(sensorDelay);
+}
+
+float mphConversion(float windSpeed) {
+  return windSpeed * 2.23694;
 }
